fc2e38c8e5eb8af59694a80b40a7537f2bcc2164
George Ofonedu <gofonedu@gmail.com>
2025-05-31 23:51:43 +0000

    you (Omitted from agent history: ...DO NOT OMITT. PLEASE APPLY THE XML CODE

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
@@ -18,6 +19,12 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'images.pexels.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
         port: '',
         pathname: '/**',
       },
